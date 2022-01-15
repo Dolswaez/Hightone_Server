0b2d57f01654e6762765f71be333817004d99a2e
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-15 18:33:51 +0900

    Initial commit

---
+++ src/main/java/com/dolswaez/hightone_server/HightoneServerApplication.java
@@ -0,0 +1,13 @@
+package com.dolswaez.hightone_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HightoneServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HightoneServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dolswaez/hightone_server/HightoneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolswaez.hightone_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HightoneServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
