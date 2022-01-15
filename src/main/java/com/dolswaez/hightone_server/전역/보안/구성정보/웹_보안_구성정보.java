ece4d7321c173bc221427c39c8293582d1a3ddde
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-16 00:54:21 +0900

    [ADD] PasswordEncoder 빈 생성

--- src/main/java/com/dolswaez/hightone_server/전역/보안/구성정보/웹_보안_구성정보.java
+++ src/main/java/com/dolswaez/hightone_server/전역/보안/구성정보/웹_보안_구성정보.java
@@ -1,8 +1,11 @@
 package com.dolswaez.hightone_server.전역.보안.구성정보;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class 웹_보안_구성정보 extends WebSecurityConfigurerAdapter {
@@ -14,5 +17,10 @@ public class 웹_보안_구성정보 extends WebSecurityConfigurerAdapter {
                 .and()
                 .cors().disable()
                 .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder 비밀번호_인코더() {
+        return new BCryptPasswordEncoder();
     }
 }
