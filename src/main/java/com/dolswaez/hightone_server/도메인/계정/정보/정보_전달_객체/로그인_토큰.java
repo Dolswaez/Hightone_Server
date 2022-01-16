e0ba4251f5b2bbdcfa261de37c89cdbea58e9a7c
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-16 12:39:22 +0900

    [FIX] 패키지 네이밍 변경 (데이터 전달 객체 -> 정보 전달 객체)

--- src/main/java/com/dolswaez/hightone_server/도메인/계정/정보/정보_전달_객체/로그인_토큰.java
+++ src/main/java/com/dolswaez/hightone_server/도메인/계정/정보/정보_전달_객체/로그인_토큰.java
@@ -1,4 +1,4 @@
-package com.dolswaez.hightone_server.도메인.계정.정보.데이터_전달_객체;
+package com.dolswaez.hightone_server.도메인.계정.정보.정보_전달_객체;
 
 public record 로그인_토큰(
         String 로그인_토큰,
--- src/main/java/com/dolswaez/hightone_server/도메인/계정/컨트롤러/회원가입_컨트롤러.java
+++ src/main/java/com/dolswaez/hightone_server/도메인/계정/컨트롤러/회원가입_컨트롤러.java
@@ -1,13 +1,7 @@
 package com.dolswaez.hightone_server.도메인.계정.컨트롤러;
 
-import com.dolswaez.hightone_server.도메인.계정.정보.데이터_전달_객체.로그인_정보;
-import com.dolswaez.hightone_server.도메인.계정.정보.데이터_전달_객체.로그인_토큰;
-import com.dolswaez.hightone_server.도메인.계정.정보.데이터_전달_객체.회원가입_정보;
-import com.dolswaez.hightone_server.도메인.계정.정보.요청.로그인_요청;
-import com.dolswaez.hightone_server.도메인.계정.정보.요청.인증_메일_송신_요청;
+import com.dolswaez.hightone_server.도메인.계정.정보.정보_전달_객체.회원가입_정보;
 import com.dolswaez.hightone_server.도메인.계정.정보.요청.회원가입_요청;
-import com.dolswaez.hightone_server.도메인.계정.정보.응답.로그인_응답;
-import com.dolswaez.hightone_server.도메인.계정.정보.응답.이메일_인증_응답;
 import com.dolswaez.hightone_server.도메인.계정.정보.응답.회원가입_응답;
 import com.dolswaez.hightone_server.도메인.계정.서비스.유저_서비스;
 import lombok.RequiredArgsConstructor;
@@ -25,13 +19,13 @@ public class 회원가입_컨트롤러 {
         String 암호화된_비밀번호 = 유저_서비스.비밀번호_암호화(요청.비밀번호());
         회원가입_정보 정보 = new 회원가입_정보(
                 요청.이름(), 요청.아이디(), 암호화된_비밀번호, 요청.이메일(),
-                요청.학교(), 요청.학년(), 요청.반(), 요청.번호()
+                요청.학교이름(), 요청.학교(), 요청.학년(), 요청.반(), 요청.번호()
         );
         Long 인덱스 = 유저_서비스.회원가입(정보);
         회원가입_응답 응답 = new 회원가입_응답(
                 인덱스,
                 정보.이름(), 정보.아이디(), 정보.암호화된_비밀번호(), 정보.이메일(),
-                정보.학교(), 정보.학년(), 정보.반(), 정보.번호()
+                정보.학교이름(), 정보.학교(), 정보.학년(), 정보.반(), 정보.번호()
         );
         return ResponseEntity.ok(응답);
     }
